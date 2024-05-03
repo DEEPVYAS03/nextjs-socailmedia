eed838b23247d3de086277828987eac5ec22a5e3
Deep Vyas <deepvyas2003@gmail.com>
2024-05-03 08:18:48 +0530

    changes

--- components/layout/TopBar.jsx
+++ components/layout/TopBar.jsx
@@ -17,7 +17,7 @@ const TopBar = () => {
   const [userData, setUserData] = useState({});
 
   const getUser = async () => {
-    const response = await fetch(`/api/user/${user.id}`);
+    const response = await fetch(`/api/user/${user?.id}`);
     const data = await response.json();
     setUserData(data);
     setLoading(false);
